1a79b198c9992ca8cf0c331e00b769b4ecece0a6
Gary A. Stafford <gary.a.stafford@gmail.com>
2021-09-10 07:55:30 -0400

    Add comments to bootstrap script

--- pyspark/emr_bootstrap/bootstrap_actions.sh
+++ pyspark/emr_bootstrap/bootstrap_actions.sh
@@ -2,8 +2,9 @@
 
 # Purpose: EMR bootstrap script
 # Author:  Gary A. Stafford
-# Date: 2021-08-31
+# Date: 2021-09-10
 
+# arg passed in by CloudFormation
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
@@ -15,7 +16,7 @@ SPARK_BUCKET=$1
 sudo yum update -y
 sudo yum install -y jq
 
-# install jsk truststore
+# jsk truststore for connecting to msk
 sudo cp /usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre/lib/security/cacerts \
   /tmp/kafka.client.truststore.jks
 
@@ -23,11 +24,10 @@ sudo cp /usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre/lib/security/cacerts 
 aws configure set region \
   "$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)"
 
-# install python packages
+# install python packages for pyspark scripts
 sudo python3 -m pip install boto3 botocore ec2-metadata
 
 # install required jars for spark
 sudo aws s3 cp \
-  "s3://${SPARK_BUCKET}/jars/" \
-  /usr/lib/spark/jars/ \
+  "s3://${SPARK_BUCKET}/jars/" /usr/lib/spark/jars/ \
   --recursive --exclude "*" --include "*.jar"
